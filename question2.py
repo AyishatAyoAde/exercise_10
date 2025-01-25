3f83f6673950d54cf9ae1f5a87e5987cdc5cf97d
Ayishat Adedeji <ayomilesi21@gmail.com>
2025-01-25 15:03:11 +0000

    EDIT: changed the correct pin from str to int

--- question2.py
+++ question2.py
@@ -1,7 +1,7 @@
 import getpass
 
 #HARDCODED PIN
-correct_PIN = '4567'
+correct_PIN = 4567
 
 max_attempts = 3
 attempts = 0
@@ -9,7 +9,7 @@ attempts = 0
 #loop for the pin
 #while the attempts is less than 3, users can input their pin
 while attempts < max_attempts:
-    supplied_pin = getpass.getpass("Please enter your PIN: ")
+    supplied_pin = input("Please enter your PIN: ")
 
     if supplied_pin == correct_PIN:
       print(f"Correct PIN in {attempts + 1} attempt")
