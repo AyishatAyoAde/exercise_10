a19b719983ac35154a8c7e04daca075483e83325
Ayishat Adedeji <ayomilesi21@gmail.com>
2025-01-25 15:01:15 +0000

    EDIT: Continued with question 1,2 and 3

--- ex10_starter.py
+++ ex10_starter.py
@@ -9,17 +9,24 @@ else:
 
 # Construct a portable wildcard pattern
 pattern = os.path.join(hdir,'*')
-search_dir = 'C:\Code\GetIntoTech\PycharmProjects\Exercise10'
+search_dir = 'C:\Code\GetIntoTech\PycharmProjects\Exercise10\*'
 
 # TODO: Use the glob.glob() function to obtain the list of filenames
 for file in glob.glob(search_dir):
-    print(search_dir)
-
+    size = os.path.getsize(file)
+    print(file, size)
+print("_" * 50)
 # TODO: use os.path.getsize to find each file's size
-size = os.path.getsize(file)
-print(size)
+
 # TODO: Add a test to only display files that are not zero length
 
-# TODO: Remove the leading directory name(s) from each filename before you print it - 
+if file != 0:
+    print(file)
+else:
+    print("file is 0")
+
+
+# TODO: Remove the leading directory name(s) from each filename before you print it -
+print(os.path.basename(file))
 # using os.path.basename()
 
